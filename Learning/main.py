3040acf6c5a064c33bf1a3d49dc133b00c49d13d
Jonatas Gomes Alves <jonatasgomes@gmail.com>
2024-05-21 23:09:26 -0400

    home

--- Learning/main.py
+++ Learning/main.py
@@ -1,3 +1,8 @@
-import nasdaqdatalink
-mydata = nasdaqdatalink.get_table('ZACKS/FC', ticker='AAPL')
-print(mydata)
+def _fib(n):
+    fib = [0, 1] + [0] * (n - 1)
+    for i in range(2, n + 1):
+        fib[i] = fib[i - 1] + fib[i - 2]
+    return fib[n]
+
+
+print(_fib(300))
