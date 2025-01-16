9e767bbec3b9f15cab0e0c5b7e35e2dea73e9c7c
Jonatas Gomes Alves <jonatasgomes@gmail.com>
2025-01-15 21:45:01 -0500

    home

--- Learning/Stock_Market/Prediction/NDX_v1/get_data.py
+++ Learning/Stock_Market/Prediction/NDX_v1/get_data.py
@@ -2,25 +2,28 @@ from datetime import datetime, timedelta
 import yfinance as yf
 import Database.database_connection as db
 
-yesterday = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
-today = datetime.now().strftime('%Y-%m-%d')
+initial = (datetime.now() - timedelta(0)).strftime('%Y-%m-%d')
+final = (datetime.now() + timedelta(1)).strftime('%Y-%m-%d')
 ndx_stocks = db.get_ndx_stocks()
 ndx_stocks.append({'ticker': '^NDX', 'id': 1, 'weight': 100})
 
-initial_date = input(f"Enter the initial date [{yesterday}]: ") or yesterday
-final_date = input(f"Enter the final date [{today}]: ") or today
+initial_date = input(f"Enter the initial date [{initial}]: ") or initial
+final_date = input(f"Enter the final date [{final}]: ") or final
+data_type = input("Enter the data type: [m]iddle day | [d]aily only | [b]oth: ") or 'm'
 
 # get daily data
-for stock in ndx_stocks:
-  timeframe = '1d'
-  data = yf.download(stock['ticker'], start=initial_date, end=final_date, interval=timeframe)
-  db.save_stock_prices(stock['id'], data, timeframe)
+if data_type == 'd' or data_type == 'b':
+  for stock in ndx_stocks:
+    timeframe = '1d'
+    data = yf.download(stock['ticker'], start=initial_date, end=final_date, interval=timeframe)
+    db.save_stock_prices(stock['id'], data, timeframe)
 
 # get 11:30am data
-for stock in ndx_stocks:
-  timeframe = '30m'
-  data = yf.download(stock['ticker'], start=initial_date, end=final_date, interval=timeframe)
-  data = data.between_time('11:30', '11:30')
-  db.save_stock_prices(stock['id'], data, timeframe)
+if data_type == 'm' or data_type == 'b':
+  for stock in ndx_stocks:
+    timeframe = '30m'
+    data = yf.download(stock['ticker'], start=initial_date, end=final_date, interval=timeframe)
+    data = data.at_time('11:30')
+    db.save_stock_prices(stock['id'], data, timeframe)
 
 print('Data saved successfully')
