d6b3a1f1e3134b2bc7c92ba7ad30b7a858306a29
Jonatas Gomes Alves <jonatasgomes@gmail.com>
2025-09-08 18:56:16 -0400

    home

--- Learning/Oracle/db_connection.py
+++ Learning/Oracle/db_connection.py
@@ -1,11 +1,11 @@
 import oracledb
-import os
 from dotenv import dotenv_values
 
 env = dotenv_values()
+print(env)
 
 with oracledb.connect(user=env['user'], password=env['pwd'], dsn=f'{env["dns"]}:{env["port"]}/{env["sid"]}') as connection:
   with connection.cursor() as cursor:
-    cursor.execute('SELECT * FROM DUAL')
+    cursor.execute('SELECT table_name FROM user_tables')
     for row in cursor:
       print(row)
